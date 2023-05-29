fc8ab9d0f2a6970da1afada7134feafc56e2ea9a
ori sharim <ori.sharim@gmail.com>
2023-05-29 19:00:22 +0300

    updated pdf calcs

--- Swerve/src/main/java/Subsystems/Swerve.java
+++ Swerve/src/main/java/Subsystems/Swerve.java
@@ -14,6 +14,7 @@ public class Swerve extends SubsystemBase{
     private AHRS m_navx = new AHRS(Port.kMXP);
     //see math on pdf document for more information
     public void drive(Supplier<Vector2d> directionVec, Supplier<Double> rotationSpeed, Supplier<Boolean> isFieldOriented){
+        
         Vector2d dirVec = directionVec.get();
         if(isFieldOriented.get()){
             dirVec = dirVec.rotate(m_navx.getAngle());
@@ -33,17 +34,21 @@ public class Swerve extends SubsystemBase{
                 maxMagnitude = rotVecs[i].mag();
             }
         }
+
         //normalize by the vector with the biggest magnitude
         for(int i = 0 ;i < rotVecs.length; i++){
             rotVecs[i].mul(1/maxMagnitude); //divide by maxMagnitude
             rotVecs[i].mul(rotationSpeed.get()); //mul by the rotation speed
         }
+        
         //add vectors
         Vector2d[] finalVecs = new Vector2d[4]; 
         for(int i = 0; i < finalVecs.length; i++){
             finalVecs[i] = new Vector2d(rotVecs[i]);
             finalVecs[i].add(dirVec);
         }
+
+        //find max magnitude
         maxMagnitude = finalVecs[0].mag();
         for(int i = 1; i < finalVecs.length; i++){
             if(finalVecs[i].mag() > maxMagnitude){
@@ -51,8 +56,10 @@ public class Swerve extends SubsystemBase{
             }
         }
 
+        //normalize by the vector with the biggest magnitude
+        for(int i = 0 ;i < finalVecs.length; i++){
+            finalVecs[i].mul(1/maxMagnitude); //divide by maxMagnitude
+        }
 
-
-    }
-    
+    }    
 }
