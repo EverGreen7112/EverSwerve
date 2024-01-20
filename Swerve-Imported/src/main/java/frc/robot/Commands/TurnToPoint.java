69127964df57afc04172a508c72181eb3c05602c
ori sharim <ori.sharim@gmail.com>
2024-01-20 13:30:59 +0200

    added TurnToPoint command

---
+++ Swerve-Imported/src/main/java/frc/robot/Commands/TurnToPoint.java
@@ -0,0 +1,30 @@
+package frc.robot.Commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.Subsystems.Swerve;
+import frc.robot.Utils.Constants;
+import frc.robot.Utils.Vector2d;
+
+public class TurnToPoint extends CommandBase implements Constants{
+    //target poiny
+    private Vector2d m_target;
+
+    public TurnToPoint(Vector2d target){
+        m_target = target;
+    }
+
+    public TurnToPoint(double targetX, double targetY){
+        m_target = new Vector2d(targetX, targetY);
+    }
+
+    @Override
+    public void execute() {
+        //get current position of robot
+        Vector2d currentPos = Swerve.getInstance(SwerveValues.USES_ABS_ENCODER).getPos();
+        //get the vector between current and target vectors
+        Vector2d deltaPos = m_target.subtract(currentPos); 
+        double targetAngle = Math.toDegrees(deltaPos.theta());
+        //rotate swerve to point
+        Swerve.getInstance(SwerveValues.USES_ABS_ENCODER).rotateTo(targetAngle);
+    }
+}
